c60ad9fa18c606b935c630199ba2d03b5e120e5a
dennisl843 <159915842+dennisl843@users.noreply.github.com>
2024-03-12 11:47:10 -0600

    Add files via upload

--- Module 5 Critical Thinking.py
+++ Module 5 Critical Thinking.py
@@ -4,6 +4,7 @@ Created on Mon Mar 11 09:20:11 2024
 
 @author: dennis.lindberg
 """
+##This portion of the program deals with Part 1 of Module 5
 ##We'll keep constants at the top
 total_rain = 0.0
 months_in_year = 12
@@ -31,9 +32,32 @@ else:
     
 print(f"\nWe collected data over a period of {months_required} months.")
 print(f"\nWe accumulated {total_rain} inches of rain over those {months_required} months.")
-print(f"\nThat means the average rainfall per month is {ave_rain} inches per month.")
+print(f"\nThat means the average rainfall per month is {ave_rain:.2f} inches per month.")
        
-       
+#%%
+
+#This program deals with Part 2 of Module 5 
+#The idea for this portion is to create a dictionary using "books_purchased" as a key value.
+#I have modified the program to make it more fair.  For example, if we didn't have any way of 
+#dealing with the values not originally defined in the dictionary, the purchaser would get nothing.
+#I am making the assumption that this is a one-time evaluation of the number of books purchased, in
+#other words, the points are awarded based on a single evaluation of the number of purchases.  It 
+#seems unfair to evaluate the number of books a single time, and if the purchaser had, say, 5 books,
+#they would still get the number of points defined for the next lowest entry in the dictionary rather than
+#returning 0, as 5 was not previously defined.  So that's why you see the dictionary different than what
+#was originally provided in the problem statement.
+
+points_per_book = {0:0, 1:0, 2:5, 3:5, 4:15, 5:15, 6:30, 7:30} 
+books_purchased = int(input('Enter the number of purchased books: '))
+
+if books_purchased >= 8:
+    points = 60
+else: points = points_per_book.get(books_purchased, 0)
+
+
+print(f"Books purchased: {books_purchased}")
+print(f"Points awarded: {points}")
+
      
      
      
